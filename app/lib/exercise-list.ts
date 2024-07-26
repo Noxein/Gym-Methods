7a05ea7d33eb70a1e349643722ae02f20f5c46e5
Noxein <138109221+Noxein@users.noreply.github.com>
2024-07-26 13:55:35 +0200

    added file for exerciselist

---
+++ app/lib/exercise-list.ts
@@ -0,0 +1,27 @@
+const exerciseList = {
+    wielostawowe:{
+        gora:[
+            "Wyciskanie sztangi na ławce poziomej",
+            "Martwy ciąg",
+            "Zwis na drążku",
+        ],
+        doł:[
+            "Martwy ciąg",
+            "Przysiad ze sztangą",
+            "AB wheel",
+            "Hip thrust"
+        ]
+    },
+    izolacyjne:{
+        ramiona:[],
+        przedramiona:[],
+        biceps:[],
+        triceps:[],
+        klata:[],
+        plecy:[],
+        nogi:[],
+        brzuch:[],
+        posladki:[],
+        lydki:[]
+    }
+}
