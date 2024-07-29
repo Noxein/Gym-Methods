c4d834c191e7ec28828e873c534cb6b8b1ac75b6
Noxein <138109221+Noxein@users.noreply.github.com>
2024-07-29 18:41:26 +0200

    added new menu at the bottom of te screen

--- app/(pages)/home/layout.tsx
+++ app/(pages)/home/layout.tsx
@@ -1,13 +1,15 @@
 import { Nav } from "@/app/components/Nav";
+import { BottomMenu } from "@/app/components/nav-menu/BottomMenu";
 
 export default function HomeLayout({children}:{children:React.ReactNode}){
     return(
         <div>
             <Nav />
             <div className="mt-20">{children}</div>
-            <div className="hidden pl-4 bg-blue-700"></div>
-            <div className="hidden pl-8"></div>
-            <div className="hidden pl-12"></div>
+            <div className="hidden ml-4 bg-blue-700"></div>
+            <div className="hidden ml-6"></div>
+            <div className="hidden ml-12"></div>
+            <BottomMenu />
         </div>
     )
 }
