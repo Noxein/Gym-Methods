eab468156735c669a27bc89e0fa7a8d5c443f189
Noxein <138109221+Noxein@users.noreply.github.com>
2024-08-12 20:35:04 +0200

    added styles for edit and delete modals

--- app/components/profile/set-tempo/DeleteTempo.tsx
+++ app/components/profile/set-tempo/DeleteTempo.tsx
@@ -1,7 +1,8 @@
 import { SelectedExerciseWithTempo } from '@/app/types'
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { BlurBackgroundModal } from '../../BlurBackgroundModal'
 import { DeleteTempoFromDb } from '@/app/actions'
+import { ThemeContext } from '@/app/context/ThemeContext'
 
 type DeleteTempoType = {
     selectedExercise: SelectedExerciseWithTempo,
@@ -10,6 +11,8 @@ type DeleteTempoType = {
 
 export const DeleteTempo = ({selectedExercise,setShowDeleteTempoModal}:DeleteTempoType) => {
     const[error,setError] = useState('')
+    const theme = useContext(ThemeContext)
+
     const HandleDeleteTempo = async () => {
         const isError = await DeleteTempoFromDb(selectedExercise.id)
         if(isError && isError.error) return setError(isError.error)
@@ -17,10 +20,12 @@ export const DeleteTempo = ({selectedExercise,setShowDeleteTempoModal}:DeleteTem
     }
   return (
     <BlurBackgroundModal onClick={()=>setShowDeleteTempoModal(false)}>
-        <div onClick={e=>e.stopPropagation()}>
-            Czy napewno chcesz usunąć tempo do ćwiczenia {selectedExercise.name}?
-            <button onClick={HandleDeleteTempo}>Usuń</button>
-            <button onClick={()=>setShowDeleteTempoModal(false)}>Anuluj</button>
+        <div onClick={e=>e.stopPropagation()} className={`flex flex-col gap-2 mb-20 bg-[${theme?.colorPallete.primary}] border-[${theme?.colorPallete.accent}] border-2 rounded-md py-6 px-10 text-xl text-[${theme?.colorPallete.accent}] max-w-xl`}>
+            <div>Czy napewno chcesz usunąć tempo do ćwiczenia <br/> <b>{selectedExercise.name}</b>?</div>
+            <div className='flex gap-2'>
+                <button onClick={HandleDeleteTempo} className='flex-1 bg-red-500 rounded-md py-2'>Usuń</button>
+                <button onClick={()=>setShowDeleteTempoModal(false)} className='flex-1 bg-gray-500 rounded-md'>Anuluj</button>
+            </div>
         </div>
     </BlurBackgroundModal>
   )
--- app/components/profile/set-tempo/EditTempo.tsx
+++ app/components/profile/set-tempo/EditTempo.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { BlurBackgroundModal } from '../../BlurBackgroundModal'
 import { SelectedExerciseWithTempo } from '@/app/types'
 import { AddOrUpdateTempo } from '@/app/actions'
+import { ThemeContext } from '@/app/context/ThemeContext'
 
 type EditTempoType = {
     selectedExercise: SelectedExerciseWithTempo,
@@ -10,6 +11,8 @@ type EditTempoType = {
 export const EditTempo = ({selectedExercise,setShowEditTempoModal}:EditTempoType) => {
     const[tempos,setTempos] = useState(selectedExercise.tempo)
     const[error,setError] = useState('')
+
+    const theme = useContext(ThemeContext)
     const handleTempoChange = (value:number,target:'up'|'uphold'|'down'|'downhold') => {
         let tempoCopy = {...tempos}
         tempoCopy[target] = value
@@ -22,17 +25,36 @@ export const EditTempo = ({selectedExercise,setShowEditTempoModal}:EditTempoType
     }
   return (
     <BlurBackgroundModal onClick={()=>setShowEditTempoModal(false)}>
-        <div onClick={e=>e.stopPropagation()}>
-            <input type="text" value={tempos.up} onChange={e=>handleTempoChange(Number(e.target.value),'up')}/>
-            <input type="text" value={tempos.uphold} onChange={e=>handleTempoChange(Number(e.target.value),'uphold')}/>
-            <input type="text" value={tempos.down} onChange={e=>handleTempoChange(Number(e.target.value),'down')}/>
-            <input type="text" value={tempos.downhold} onChange={e=>handleTempoChange(Number(e.target.value),'downhold')}/>
-            <div>
-                <button onClick={HandleAddOrUpdateTempo}>Zapisz</button>
-                <button onClick={()=>setShowEditTempoModal(false)}>Anuluj</button>
+        <div onClick={e=>e.stopPropagation()} className={`flex flex-col gap-2 text-xl px-10 mb-20 py-5 bg-[${theme?.colorPallete.primary}] text-[${theme?.colorPallete.accent}] border-[${theme?.colorPallete.accent}] border-2 rounded-md max-w-xl`}>
+            <div>Tempo - {selectedExercise.name}</div>
+            <div className='flex flex-col gap-2'>
+                <div className='flex gap-4'>
+                    <DivInputLabel handleChange={handleTempoChange} text='Tempo Góra' value={tempos.up} target='up'/>
+                    <DivInputLabel handleChange={handleTempoChange} text='Wstrzymanie Góra' value={tempos.uphold} target='uphold'/>
+                </div>
+                <div className='flex gap-4'>
+                    <DivInputLabel handleChange={handleTempoChange} text='Tempo Dół' value={tempos.down} target='down'/>
+                    <DivInputLabel handleChange={handleTempoChange} text='Wstrzymanie Dół' value={tempos.downhold} target='downhold'/>
+                </div>
+            </div>
+            
+            <div className='flex gap-2'>
+                <button onClick={HandleAddOrUpdateTempo} className='rounded-md flex-1 bg-green-500 mx-6 py-2'>Zapisz</button>
+                <button onClick={()=>setShowEditTempoModal(false)} className='rounded-md flex-1 bg-red-500 mx-6'>Anuluj</button>
             </div>
             {error && <div className='text-red-600'>{error}</div>}
         </div>
     </BlurBackgroundModal>
   )
+}
+
+const DivInputLabel = ({handleChange,value,text,target}:{handleChange:(value:number,target:'up'|'uphold'|'down'|'downhold')=>void,value:number,text:string,target:'up'|'uphold'|'down'|'downhold'}) => {
+    const theme = useContext(ThemeContext)
+
+    return (
+        <div className='flex-1'>
+            <label htmlFor={text}>{text}</label>
+            <input type="text" id={text} value={value} onChange={e=>handleChange(Number(e.target.value),target)} className={`w-full bg-[${theme?.colorPallete.primary}] text-[${theme?.colorPallete.accent}] border-[${theme?.colorPallete.accent}] border-2 rounded-md pl-2 py-1`}/>
+        </div>
+    )
 }
