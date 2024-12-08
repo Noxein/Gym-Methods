d07ff72e9412ddf176c77fa8e114efd7418e3073
Noxein <138109221+Noxein@users.noreply.github.com>
2024-12-08 13:54:27 +0100

    fixed language not being selected from localstorage issue

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,10 +1,17 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SelectLanguage } from './components/SelectLanguage'
+import { getLang } from './lib/utils'
 
 export const Main = ({children}:{children:React.ReactNode}) => {
     const lang = localStorage.getItem('lang')
     const showSelectLang = lang
+
+    useEffect(()=>{
+      if(showSelectLang){
+        getLang()
+      }
+    },[])
   return (
     <div>
         {showSelectLang ? children : <SelectLanguage /> }
