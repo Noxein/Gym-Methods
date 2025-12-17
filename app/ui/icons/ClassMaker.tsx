cbdb6149b0d68fa7c57662cc72fcf2d895418858
Noxein <138109221+Noxein@users.noreply.github.com>
2025-12-17 14:32:18 +0100

    new mail commit

--- app/ui/icons/ClassMaker.tsx
+++ app/ui/icons/ClassMaker.tsx
@@ -5,7 +5,7 @@ export const ClassMaker = () => {
         <div className={`text-[#576CA8] bg-[#576CA8] border-[#576CA8]`}>LightMode</div>
         <div className={`text-[#0D1317] bg-[#E7E7E7] border-b-[#576CA8] bg-dark text-marmur ml-10`}>DarkMode</div>
         <div className="hidden ml-4 bg-blue-700"></div>
-            <div className="hidden ml-6"></div>
+            <div className="hidden ml-6 mb-2"></div>
             <div className="hidden ml-12 text-dark"></div>
     </div>
   )
