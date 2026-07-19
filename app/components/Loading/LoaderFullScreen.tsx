d3db66aa2a309f99a5768e6520ceae95aa5b60c6
Noxein <138109221+Noxein@users.noreply.github.com>
2026-07-19 18:54:53 +0200

    now loading image is visible insead of its alt

--- app/components/Loading/LoaderFullScreen.tsx
+++ app/components/Loading/LoaderFullScreen.tsx
@@ -4,7 +4,7 @@ export const LoaderFullScreen = () => {
   return(
     <div className="loading loading02 h-screen w-mobile max-w-mobile flex items-center justify-center">
     <span className='flex justify-center'>
-      <Image className='mb-20 mx-auto'  width='300' height='200' src="/mordor.png" alt="Gothic fonts"/>
+      <Image className='mb-20 mx-auto'  width='300' height='200' src="/mordor.png" alt="Gothic fonts" unoptimized />
     </span>
   </div>
   )
